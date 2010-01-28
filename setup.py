b4bdbd583bfabf47b75ead10dcc3efab43ca2bed
Richard Crowley <r@rcrowley.org>
2010-01-28 01:09:11 +0000

    Dependencies in setup.py.

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ distutils.core.setup(
     name="spoon",
     version="0.1",
     packages=["spoon"],
+    requires=["html5lib"],
     author="Richard Crowley",
     author_email="r@rcrowley.org",
     url="http://github.com/rcrowley/spoon",
     license="BSD",
-    description="Spoon feeds HTML5 into Atom."
+    description="Spoon feeds HTML5 into Atom.",
 )
